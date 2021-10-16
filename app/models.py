5fcaff76819a6e818afbc4da3ab658a3ccd0f80f
ken <ken.mbira@student.moringaschool.com>
2021-10-16 12:44:38 +0300

    set up a test for the instance of the user

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    """This will define all the components of a user's profile
+
+    Args:
+        models ([type]): [description]
+    """
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    username = models.CharField(max_length=50)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    bio = models.TextField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile/')
+
+
+
+
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Profile
+from django.contrib.auth.models import User
+
+class TestProfile(TestCase):
+    """This tests the behaviours of the profile class
+
+    Args:
+        TestCase ([type]): [description]
+    """
+    def setUp(self):
+        """This will run before every test case
+        """
+        self.user = User.objects.create(username='mbira')
+        self.user.save()
+        self.user_profile = Profile(user = self.user,username='kenmbesh',bio='my bio',profile_pic='image')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user_profile,Profile))
+
+    # def tearDown(self):
+    #     """This will clear the database after every test case
+    #     """
+    #     self.user.delete()
